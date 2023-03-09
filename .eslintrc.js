d3a06f21be4dfba9121ee2b9518e76529e0d92a7
gongxun <gongxun@yccjk.com>
2023-03-09 15:56:25 +0800

    feat: init

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ["taro/react", "plugin:prettier/recommended"],
+  rules: {
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "prettier/prettier": "error",
+  },
+};
---
+++ config/index.js
@@ -0,0 +1,82 @@
+const path = require('path');
+
+const config = {
+  projectName: "nk-react-hooks",
+  date: "2023-3-9",
+  designWidth: 750,
+  deviceRatio: {
+    640: 2.34 / 2,
+    750: 1,
+    828: 1.81 / 2,
+    375: 2 / 1
+  },
+  sourceRoot: "src",
+  outputRoot: "dist",
+  plugins: ['@tarojs/plugin-html'],
+  defineConstants: {},
+  copy: {
+    patterns: [],
+    options: {},
+  },
+  framework: "react",
+  compiler: 'webpack5',
+  cache: {
+    enable: true // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+  },
+  sass:{
+    resource: [
+      path.resolve(__dirname, '..', 'src/assets/styles/custom_theme.scss')
+    ],
+    data: `@import "@nutui/nutui-react-taro/dist/styles/variables.scss";`
+  },
+  pxtransform: {
+    config: {
+      selectorBlackList: ['nut-']
+    }
+  },
+  mini: {
+    postcss: {
+      pxtransform: {
+        enable: true,
+        config: {},
+      },
+      url: {
+        enable: true,
+        config: {
+          limit: 1024, // 设定转换尺寸上限
+        },
+      },
+      cssModules: {
+        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        config: {
+          namingPattern: "module", // 转换模式，取值为 global/module
+          generateScopedName: "[name]__[local]___[hash:base64:5]",
+        },
+      },
+    },
+  },
+  h5: {
+    publicPath: "/",
+    staticDirectory: "static",
+    postcss: {
+      autoprefixer: {
+        enable: true,
+        config: {},
+      },
+      cssModules: {
+        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        config: {
+          namingPattern: "module", // 转换模式，取值为 global/module
+          generateScopedName: "[name]__[local]___[hash:base64:5]",
+        },
+      },
+    },
+  },
+};
+
+module.exports = function (merge) {
+  if (process.env.NODE_ENV === "development") {
+    return merge({}, config, require("./dev"));
+  }
+  return merge({}, config, require("./prod"));
+};
